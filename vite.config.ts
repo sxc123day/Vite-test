a0ff02009be1b41396651fdf300d0387370c69ed
shixiaochuang <shixiaochuang0722@163.com>
2021-05-08 15:45:44 +0800

    优化

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,28 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import styleImport from 'vite-plugin-style-import'
 
 import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    styleImport({
+      libs: [{
+        libraryName: 'element-plus',
+        esModule: true,
+        ensureStyleFile: true,
+        resolveStyle: (name) => {
+          name = name.slice(3)
+          return `element-plus/packages/theme-chalk/src/${name}.scss`;
+        },
+        resolveComponent: (name) => {
+          return `element-plus/lib/${name}`;
+        },
+      }]
+    })
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
